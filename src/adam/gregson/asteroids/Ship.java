f1c0e9fa97cacdb856410c3e570fa8e415078c40
mr-gregson <agregson@utschools.ca>
2023-02-12 16:07:25 -0500

    added static section to load images

--- src/adam/gregson/asteroids/Ship.java
+++ src/adam/gregson/asteroids/Ship.java
@@ -1,11 +1,35 @@
 package adam.gregson.asteroids;
 
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Ship extends Sprite{
+
+	private static final int ROTATIONS = 64;
+	private static final BufferedImage[] images;
+
+	static{
+		images = new BufferedImage[ROTATIONS];
+		for (int i = 0; i < ROTATIONS; i++)
+		try{
+			images[i] = ImageIO.read(new BufferedInputStream(new FileInputStream(String.format("img/ship/ship00%02d.png", i))));
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+	}
 	
 	
-	public Ship(BufferedImage[] images, double x, double y) {
+	public Ship(double x, double y) {
 		super(images, x, y);
+	}
+
+	public static void main(String[] args) {
+		Ship ship = new Ship(0, 0);
+
 	}
 }
