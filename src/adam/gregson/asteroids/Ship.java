3acdf5abe57a1884dd50684969925ff62e3f113f
mr-gregson <agregson@utschools.ca>
2023-02-12 13:48:05 -0500

    updated ship sprite added two images for ship sprite

--- src/adam/gregson/asteroids/Ship.java
+++ src/adam/gregson/asteroids/Ship.java
@@ -2,29 +2,10 @@ package adam.gregson.asteroids;
 
 import java.awt.image.BufferedImage;
 
-import adam.gregson.mycommonmethods.FileIO;
-
-
-public class Ship {
+public class Ship extends Sprite{
 	
-	private static final String FILE_NAME = "./PNG/playerShip1_red.png";
-	private static BufferedImage image;
 	
-	private static int WIDTH;
-	private static int HEIGHT;
-
-	private static double directionX = 0.0;
-	private static double directionY = 1.0;
-
-	private static double velocityX = 0.0;
-	private static double velocityY = 0.0;
-
-	
-	public Ship() {
-		image = FileIO.readImageFile(this, FILE_NAME);
-		
-		
+	public Ship(BufferedImage[] images, double x, double y) {
+		super(images, x, y);
 	}
-	
-
 }
--- src/adam/gregson/asteroids/Sprite.java
+++ src/adam/gregson/asteroids/Sprite.java
@@ -1,11 +1,80 @@
 package adam.gregson.asteroids;
 
-public class Sprite {
+import java.awt.image.BufferedImage;
+
+public abstract class Sprite {
 	
-	private int direction = 90;
-	private int x;
-	private int y;
-	private int dx;
-	private int dy;
+	private BufferedImage[] images;
+	private double dx;
+	private double dy;
+	private int currentImage;
+	private double angle;
+	private double x;
+	private double y;
+
+	
+	public Sprite(BufferedImage[] images, double x, double y) {
+		this.images = images;
+		this.dx = 0;
+		this.dy = 0;
+		this.currentImage = 0;	
+		this.x = x;
+		this.y = y;
+
+		this.angle = 2*Math.PI / images.length;
+	}
+	
+
+	public void rotateLeft(){
+		currentImage--;
+		if (currentImage < 0) {
+			currentImage = images.length - 1;
+		}
+		double tempX = Math.cos(angle)*dx - Math.sin(angle)*dy;
+		double tempY = Math.sin(angle)*dx + Math.cos(angle)*dy;
+		dx = tempX;
+		dy = tempY;
+	}
+
+	public void rotateRight(){
+		currentImage++;
+		if (currentImage >= images.length) {
+			currentImage = 0;
+		}
+		double tempX = Math.cos(-angle)*dx - Math.sin(-angle)*dy;
+		double tempY = Math.sin(-angle)*dx + Math.cos(-angle)*dy;
+		dx = tempX;
+		dy = tempY;
+	}
+
+	public void accelerate(double acceleration) {
+		dx += acceleration * Math.cos(currentImage * angle);
+		dy += acceleration * Math.sin(currentImage * angle);
+	}
+
+	public void move() {
+		x += dx;
+		y += dy;
+	}
+
+	public BufferedImage getImage() {
+		return images[currentImage];
+	}
+
+	public double getX() {
+		return x;
+	}	
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}	
+
+	public void setY(double y) {
+		this.y = y;
+	}
 
 }
